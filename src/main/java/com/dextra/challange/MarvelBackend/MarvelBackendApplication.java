0dca0a11af9190eb183abfaff4ea7c444679a9fe
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-10-18 12:22:42 -0300

    init

---
+++ src/main/java/com/dextra/challange/MarvelBackend/MarvelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dextra.challange.MarvelBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dextra/challange/MarvelBackend/MarvelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dextra.challange.MarvelBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
